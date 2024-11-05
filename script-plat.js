9b2dcbee174b04a64e8492f6de0ee9336207a960
AuroreTnr <tournieraurore@orange.fr>
2024-11-05 16:03:31 +0100

    filtre fait sur la page plat mais erreur de frappe pas encore implémentée

---
+++ script-plat.js
@@ -0,0 +1,31 @@
+
+const input = document.querySelector(".search-input");
+const listItem = document.querySelectorAll("#list-item");
+
+
+
+function nettoyerMot(mot) {
+    return mot.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  }
+  
+
+
+input.addEventListener("input", filterItems);
+
+function filterItems(){
+    const mot = this.value;
+    let motUtilisateur = nettoyerMot(mot).toLowerCase();
+
+    listItem.forEach(card => {
+      let attribut = card.getAttribute("data-key").toLocaleLowerCase();
+              
+      let found = attribut.includes(motUtilisateur) ? card.style.display="" : card.style.display="none";
+
+    });
+
+    
+}
+
+// encore la gestion des erreur de saisie de l utilisateur => trouver une bibliotheque facile à implémenter ?
+
+
--- script.js
+++ script.js
@@ -1,63 +1,7 @@
+const div = document.querySelector(".container-filter");
 const input = document.querySelector(".search-input");
-const cards = document.querySelectorAll(".main-card");
-const nameFood = document.querySelectorAll(".name-food");
 
-
-input.addEventListener("input", filterItems);
-
-function filterItems() {
-    const filter = this.value.toLowerCase();
-    console.log(filter);
-
-
-
-    nameFood.forEach(nameFood => {
-
-        console.log(nameFood.textContent);
-
-        let found = false;
-
-        if (nameFood.textContent.toLowerCase().includes(filter)) {
-            found = true;
-        }
-
-        cards.forEach(card => {
-            card.style.display = found ? '' : 'none'
-        })
-
-
-
-
-        // let valeur = found ? console.log("ok") : console.log("pas ok");
-
-
-
-        // cards.style.display = found ? '' : 'none';
-    });
-}
-
-
-
-// Filtrer les éléments de la liste
-// input.addEventListener('input', filterItems);
-
-// function filterItems() {
-//     const filter = this.value.toLowerCase();
-//     const rows = tableBody.querySelectorAll('tr');
-
-//     rows.forEach(row => {
-//         const cells = row.querySelectorAll("td")
-//         let found = false;
-
-//         cells.forEach(cell => {
-//             if (cell.textContent.toLowerCase().includes(filter)) {
-//                 found = true;
-//             }
-//         });
-
-//         row.style.display = found ? '' : 'none';
-//     });
-// };
+console.log(div);
 
 
 
