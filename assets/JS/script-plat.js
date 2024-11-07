7206450a9daccbbbe9730b66db1bd7289eb749d5
AuroreTnr <tournieraurore@orange.fr>
2024-11-07 15:40:52 +0100

    basique fini

--- assets/JS/script-plat.js
+++ assets/JS/script-plat.js
@@ -1,11 +1,12 @@
 
 const input = document.querySelector(".search-input");
-const listItem = document.querySelectorAll("#list-item");
+const listItem = document.querySelectorAll(".food-card");
+
 
 
 //nettoie la chaine
 function nettoyerMot(mot) {
-    return mot.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    return mot.replace(/[^a-zA-Z][ ]/g, '').toLowerCase();
   }
   
 
@@ -17,6 +18,7 @@ function filterItems(){
     let motUtilisateur = nettoyerMot(mot).toLowerCase();
 
     listItem.forEach(card => {
+      
       let attribut = card.getAttribute("data-key").toLocaleLowerCase();
               
       let found = attribut.includes(motUtilisateur) ? card.style.display="" : card.style.display="none";
