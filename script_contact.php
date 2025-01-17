b56a24833eb8b9bb3693906672faab53247f0f57
Aurore <tournieraurore@orange.fr>
2025-01-17 19:08:21 +0100

    modification script contact

--- script_contact.php
+++ script_contact.php
@@ -12,45 +12,48 @@ $_SESSION['attributes'] = "alert-danger";
 // Vérification de la soumission du formulaire
 if (isset($_POST['ok'])) {
 
-    // Validation du nom (au moins 1 caractère et que des lettres)
-    if (empty($_POST['nom']) || !preg_match('/^[A-Za-zÀ-ÖØ-öø-ÿ\-]+$/', $_POST['nom'])) {
-        $_SESSION['error_message'] = "Entrez un nom valide (lettres seulement).";
-        $_SESSION['nom'] = $_POST['nom'];
-        header("Location: contact.php");
-        exit;
-    }else {
-        $_SESSION['nom'] = $_POST['nom'];
-    }
-
-    // Validation du prénom (au moins 1 caractère et que des lettres)
-    if (empty($_POST['prenom']) || !preg_match('/^[A-Za-zÀ-ÖØ-öø-ÿ\-]+$/', $_POST['prenom'])) {
-        $_SESSION['error_message'] = "Entrez un prénom valide (lettres seulement).";
-        $_SESSION['prenom'] = $_POST['prenom'];
-        header("Location: contact.php");
-        exit;
-    }else{
-        $_SESSION['prenom'] = $_POST['prenom'];
-    }
-
-    // Validation de l'email
-    if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        $_SESSION['error_message'] = "Entrez un email valide.";
-        $_SESSION['email'] = $_POST['email'];
-        header("Location: contact.php");
-        exit;
-    }else{
-        $_SESSION['email'] = $_POST['email'];
-    }
-
     // Validation du commentaire (au moins 1 caractère)
-    if (empty($_POST['commentaire'])) {
+    // il est en premier pour pas qu il s efface si l email n' est pas bon. 
+    if (!empty($_POST['commentaire'])) {
+        $_SESSION['commentaire'] = $_POST['commentaire'];
+    }else{
         $_SESSION['error_message'] = "Entrez un commentaire.";
         $_SESSION['commentaire'] = $_POST['commentaire'];
         header("Location: contact.php");
         exit;
-    }else{
-        $_SESSION['commentaire'] = $_POST['commentaire'];
     }
+    
+
+    // Validation du nom (au moins 1 caractère et que des lettres)
+    if (!empty($_POST['nom']) && preg_match('/^[A-Za-zÀ-ÖØ-öø-ÿ\-]+$/', $_POST['nom'])) {
+        $_SESSION['nom'] = $_POST['nom'];
+    }else {
+        $_SESSION['error_message'] = "Entrez un nom valide (lettres seulement).";
+        $_SESSION['nom'] = $_POST['nom'];
+        header("Location: contact.php");
+        exit;
+    }
+
+    // Validation du prénom (au moins 1 caractère et que des lettres)
+    if (!empty($_POST['prenom']) && preg_match('/^[A-Za-zÀ-ÖØ-öø-ÿ\-]+$/', $_POST['prenom'])) {
+        $_SESSION['prenom'] = $_POST['prenom'];
+    }else{
+        $_SESSION['error_message'] = "Entrez un prénom valide (lettres seulement).";
+        $_SESSION['prenom'] = $_POST['prenom'];
+        header("Location: contact.php");
+        exit;
+    }
+
+    // Validation de l'email
+    if (!empty($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['email'] = $_POST['email'];
+    }else{
+        $_SESSION['error_message'] = "Entrez un email valide.";
+        $_SESSION['email'] = $_POST['email'];
+        header("Location: contact.php");
+        exit;
+    }
+
 
     try {
         $mail = new PHPMailer(true);
@@ -75,13 +78,17 @@ if (isset($_POST['ok'])) {
         // Envoi de l'email
         $mail->send();
 
-        if ($mail) {
+        if (isset($mail)) {
             $_SESSION['error_message'] = "Votre message a été envoyé avec succès !";
             $_SESSION['attributes'] = "alert-success";
 
+            unset($_SESSION['nom']);
+            unset($_SESSION['prenom']);
+            unset($_SESSION['email']);
+            unset($_SESSION['commentaire']);
+
         }
 
-        // Redirection vers la page de contact avec un message de succès
         header("Location: contact.php");
         exit;
 
